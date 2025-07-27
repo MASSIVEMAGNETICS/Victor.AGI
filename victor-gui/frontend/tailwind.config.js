d19959aca8a0e9f9974ba33d5741c1705c3c8998
IAMBANDOBANDZ <massivemagnetics@gmail.com>
2025-07-27 11:13:35 -0400

    Add experimental Paradigm Shatterer model

---
+++ victor-gui/frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        background: '#0d0f1a',
+        cyan: '#00e7ff',
+        magenta: '#ff007d',
+        violet: '#7b2cff'
+      }
+    }
+  },
+  plugins: []
+};
